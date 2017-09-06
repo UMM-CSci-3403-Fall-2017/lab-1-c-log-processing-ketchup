4ee42eb2ea2e9273f7a4adecf610b374c04f4641
Rocherno De Jongh <dejon076@falcon.morris.umn.edu>
2017-09-06 00:15:49 -0500

    awk prints out correct format for process_client_logs.sh

--- bin/process_client_logs.sh
+++ bin/process_client_logs.sh
@@ -5,7 +5,7 @@ cd ../$1
 
 cat * > temp.txt
 
-awk '{print $1,$2,`grep [^:]* $3`,$(NF-3), $(NF-5);}' temp.txt > failed_login_data.txt
+awk '{print $1,$2,substr($3,0,2),$(NF-5), $(NF-3);}' temp.txt > failed_login_data.txt
 
 
 
