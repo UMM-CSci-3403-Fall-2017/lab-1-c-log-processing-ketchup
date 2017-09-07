3d2db2ae6b4ad61f1dd19ba5de3b20bfacb44bab
Rocherno De Jongh <dejon076@acrylic.morris.umn.edu>
2017-09-07 16:18:41 -0500

    Every test passes

--- bin/process_client_logs.sh
+++ bin/process_client_logs.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-#cd $1
+cat `grep -rl "Failed password for" $1` > temp.txt
 
-#cat * > temp.txt
+grep -r "Failed password for" temp.txt > temp2.txt
 
-grep -R "Failed password for" $1 > temp1.txt
+awk '{print $1,$2,substr($3,0,2),$(NF-5),$(NF-3);}' temp2.txt > $1/failed_login_data.txt
 
-#rm temp.txt
+rm temp.txt
+rm temp2.txt
 
-awk '{print $1,$2,substr($3,0,2),$(NF-5),$(NF-3);}' temp1.txt > failed_login_data.txt
-
-rm temp1.txt
