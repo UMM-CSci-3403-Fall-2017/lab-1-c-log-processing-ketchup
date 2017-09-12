eb0815ac0a365bb9ed8b14ad6d42e14122ed7346
tszhonglau <lauxx265@morris.umn.edu>
2017-09-12 11:45:22 -0500

    Every test passed for assemble_report

--- bin/assemble_report.sh
+++ bin/assemble_report.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cat $1/username_dist.html $1/hours_dist.html $1/country_dist.html > temp.txt
+cat $1/country_dist.html $1/hours_dist.html $1/username_dist.html > temp.txt
 
 ./bin/wrap_contents.sh temp.txt ./html_components/summary_plots $1/failed_login_summary.html
 
