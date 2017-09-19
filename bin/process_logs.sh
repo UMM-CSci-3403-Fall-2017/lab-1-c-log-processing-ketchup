51f5f6610cd87b489bfa35279db4777672695558
Rocherno De Jongh <dejon076@normandy.morris.umn.edu>
2017-09-19 11:28:22 -0500

    script succesfully calls process_client_logs.sh on each client's set of logs through loop

--- bin/process_logs.sh
+++ bin/process_logs.sh
@@ -2,14 +2,14 @@
 
 SCRATCH=`mktemp -d`
 home=$(pwd)
-cd $SCRATCH
 
 for arg in "$@"
 do 
 	tempname=$(basename $arg)
 	name=$(echo $tempname | cut -f 1 -d '_')
-	mkdir $name
-	tar -zxf $home/$arg -C $name
+	mkdir $SCRATCH/$name
+	tar -zxf $home/$arg -C $SCRATCH/$name
+	./bin/process_client_logs.sh $SCRATCH/$name
 
 done
 
